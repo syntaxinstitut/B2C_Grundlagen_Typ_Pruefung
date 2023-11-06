4b150002be95942a832756e07497c0a9d837c73d
luc <luc.spielvogel@syntax-institut.de>
2023-11-06 20:54:39 +0100

    first solution commit

--- Aufgabe1/src/Main.kt
+++ Aufgabe1/src/Main.kt
@@ -1,9 +1,19 @@
 fun main() {
     val meinAuto = Auto("Volkswagen", "Golf")
     val meinFahrzeug = Fahrzeug("Fahrrad")
-
+    pruefeFahrzeugUndDruckeDetails(meinAuto)
+    pruefeFahrzeugUndDruckeDetails(meinFahrzeug)
+    pruefeFahrzeugUndDruckeDetails(null)
 }
 
-fun prüfeFahrzeugUndDruckeDetails(fahrzeug: Fahrzeug) {
+fun pruefeFahrzeugUndDruckeDetails(fahrzeug: Fahrzeug?) {
     // Füge hier deine Implementierung ein.
+    if (fahrzeug is Auto) {
+        // Dank Smart Cast kann direkt auf die Eigenschaft modell zugegriffen werden.
+        println("Das Auto ist ein ${fahrzeug.marke} ${fahrzeug.modell}.")
+    } else if (fahrzeug != null) {
+        println("Das ist kein Auto, sondern ein anderes Fahrzeug der Marke ${fahrzeug.marke}.")
+    } else {
+        println("Das Fahrzeug ist null und kann nicht geprüft werden.")
+    }
 }
