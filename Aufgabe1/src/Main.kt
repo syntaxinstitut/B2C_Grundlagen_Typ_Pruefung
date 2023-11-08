3d098d76eda84c4d831aadd55ec2a221f9256b61
luc <luc.spielvogel@syntax-institut.de>
2023-11-08 21:23:05 +0100

    second solution commit

--- Aufgabe1/src/Main.kt
+++ Aufgabe1/src/Main.kt
@@ -4,6 +4,8 @@ fun main() {
     pruefeFahrzeugUndDruckeDetails(meinAuto)
     pruefeFahrzeugUndDruckeDetails(meinFahrzeug)
     pruefeFahrzeugUndDruckeDetails(null)
+    pruefeUndCasteFahrzeug(meinAuto)
+    pruefeUndCasteFahrzeug(meinFahrzeug)
 }
 
 fun pruefeFahrzeugUndDruckeDetails(fahrzeug: Fahrzeug?) {
@@ -15,5 +17,19 @@ fun pruefeFahrzeugUndDruckeDetails(fahrzeug: Fahrzeug?) {
         println("Das ist kein Auto, sondern ein anderes Fahrzeug der Marke ${fahrzeug.marke}.")
     } else {
         println("Das Fahrzeug ist null und kann nicht geprüft werden.")
+    }
+}
+
+// Funktion, die ein 'Fahrzeug' Objekt entgegennimmt und versucht, es sicher in ein 'Auto' zu casten
+fun pruefeUndCasteFahrzeug(fahrzeug: Fahrzeug) {
+    // Versuche das Fahrzeug-Objekt zu einem Auto-Objekt zu casten
+    val auto = fahrzeug as? Auto
+    // Überprüfe, ob der Cast erfolgreich war und gib entsprechendes Feedback aus
+    if (auto != null) {
+        // Cast erfolgreich, gib die Marke und das Modell des Autos aus
+        println("Das Fahrzeug ist ein Auto der Marke ${auto.marke} und dem Modell ${auto.modell}.")
+    } else {
+        // Cast nicht erfolgreich, gib eine Meldung aus, dass das Fahrzeug kein Auto ist
+        println("Das Fahrzeug ist kein Auto.")
     }
 }
